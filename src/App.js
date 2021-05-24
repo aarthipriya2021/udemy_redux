02199ce1ab73ea020686973bceaf8dd8ce25f78f
aarthipriya2021 <a.aarthipriya2021@gmail.com>
2021-05-24 19:56:43 +0530

    redux actions added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Counter from './components/Counter';
-
+import store from './redux/store';
+import { Provider} from 'react-redux'
 
 function App() {
   return (
-    <Counter />
+    <Provider store={store}>
+      <div>
+        <Counter /> 
+      </div>
+       
+    </Provider>
+    
       
   );
 }
